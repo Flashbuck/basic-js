78af0e366a63c20e6412e2fcef834929510c51ec
Flashbuck <sprincyn@gmail.com>
2020-11-01 02:01:03 +0300

    feat: add last script

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,16 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
+module.exports = function dateSample(sampleActivity = undefined) {
+  let result = Math.log(MODERN_ACTIVITY/parseInt(sampleActivity))/(0.693/HALF_LIFE_PERIOD);
+  if(typeof sampleActivity !== 'string' || 
+  isNaN(sampleActivity) || 
+  isNaN(result) ||
+  sampleActivity == undefined || 
+  parseInt(sampleActivity) > 15 || 
+  parseInt(sampleActivity) <= 0){
+    return false;
+  }
+  return Math.ceil(result);
   // remove line with error and write your code here
 };
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,71 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(modif = true) {
+    this.modif = modif;
+    this.alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  }
+
+  encrypt(message, key) {
+    if (!(message && key)) throw "Error";
+
+    let newMessage = message.toUpperCase();
+    let newKey = "";
+    let j = 0;
+    for (let i = 0; i < message.length; i++) {
+      if (this.alphabet.includes(newMessage[i])) {
+        newKey += key[j % key.length]
+        j++;
+      } else {
+        newKey += message[i];
+      }
+    }
+    newKey = newKey.toUpperCase();
+    let res = "";
+    for (let i = 0; i < message.length; i++) {
+      if (this.alphabet.includes(newMessage[i])) {
+        res += String.fromCharCode(((newMessage.charCodeAt(i) + newKey.charCodeAt(i)) - 130) % 26 + 65);
+      } else {
+        res += newMessage[i];
+      }
+    }
+
+    if (this.modif) {
+      return res;
+    } else {
+      return res.split("").reverse().join("");
+    }
+  }
+
+  decrypt(message, key) {
+    if (!(message && key)) throw "Error";
+
+    let newMessage = message.toUpperCase();
+    let newKey = "";
+    let j = 0;
+    for (let i = 0; i < message.length; i++) {
+      if (this.alphabet.includes(newMessage[i])) {
+        newKey += key[j % key.length]
+        j++;
+      } else {
+        newKey += message[i];
+      }
+    }
+    newKey = newKey.toUpperCase();
+    let res = "";
+    for (let i = 0; i < message.length; i++) {
+      if (this.alphabet.includes(newMessage[i])) {
+        res += String.fromCharCode((newMessage.charCodeAt(i) + 26 - newKey.charCodeAt(i))  % 26 + 65);
+      } else {
+        res += newMessage[i];
+      }
+    }
+
+    if (this.modif) {
+      return res;
+    } else {
+      return res.split("").reverse().join("");
+    }
   }
 }
 
